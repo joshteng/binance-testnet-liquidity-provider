b93a2c60554dce6b1bdcb9b28c1b616ae6dfeb50
Josh Teng <joshteng@me.com>
2022-11-30 18:59:23 +0800

    test trade with either no base asset or no quote asset

--- tests/bot/test_testnet_mm.py
+++ tests/bot/test_testnet_mm.py
@@ -45,14 +45,48 @@ def test_trade_without_funds(**kwargs):
         TestnetMMState.PRODUCTION_LAST_PRICE = '1'
         mm._trade()
 
+@rm.Mocker(kw='mock')
+def test_trade_with_quote_asset_and_no_base_asset(**kwargs):
+    from bot.testnet_mm import TestnetMM
+    from bot.testnet_mm_state import TestnetMMState
+    from unittest.mock import MagicMock
+
+    kwargs['mock'].get(rm.ANY, json=MOCK_RESPONSES['getAccountWithoutBase'])
+
+    base_asset, quote_asset = 'BTC', 'BUSD'
+
+    mm = TestnetMM(base_asset, quote_asset, 'key', 'secret')
+    mm._buy_base_asset = MagicMock()
+
+    base_asset_qty, quote_asset_qty = mm._get_balances(base_asset, quote_asset)
+    assert float(quote_asset_qty) > 0
+    assert float(base_asset_qty) == 0
+
+    TestnetMMState.PRODUCTION_LAST_PRICE = '1'
+    mm._trade()
+    assert mm._buy_base_asset.called
+
 
 @rm.Mocker(kw='mock')
 def test_trade_with_base_asset_and_no_quote_asset(**kwargs):
-    pass
+    from bot.testnet_mm import TestnetMM
+    from bot.testnet_mm_state import TestnetMMState
+    from unittest.mock import MagicMock
 
-@rm.Mocker(kw='mock')
-def test_trade_with_quote_asset_and_no_base_asset(**kwargs):
-    pass
+    kwargs['mock'].get(rm.ANY, json=MOCK_RESPONSES['getAccountWithoutQuote'])
+
+    base_asset, quote_asset = 'BTC', 'BUSD'
+
+    mm = TestnetMM(base_asset, quote_asset, 'key', 'secret')
+    mm._buy_quote_asset = MagicMock()
+
+    base_asset_qty, quote_asset_qty = mm._get_balances(base_asset, quote_asset)
+    assert float(quote_asset_qty) == 0
+    assert float(base_asset_qty) > 0
+
+    TestnetMMState.PRODUCTION_LAST_PRICE = '1'
+    mm._trade()
+    assert mm._buy_quote_asset.called
 
 @rm.Mocker(kw='mock')
 def test_truncate_price(**kwargs):
@@ -61,3 +95,12 @@ def test_truncate_price(**kwargs):
 @rm.Mocker(kw='mock')
 def test_truncate_quantity(**kwargs):
     pass
+
+@rm.Mocker(kw='mock')
+def test_buy_quote_asset(**kwargs):
+    pass
+
+@rm.Mocker(kw='mock')
+def test_buy_base_asset(**kwargs):
+    pass
+
